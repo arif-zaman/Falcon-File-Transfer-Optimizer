0d5293185af7dbaf2922be59f1025c4d6ebcf3ce
Md Arifuzzaman <arif@nevada.unr.edu>
2020-08-31 15:53:42 -0700

    gp hedge

--- search.py
+++ search.py
@@ -20,7 +20,7 @@ def initial_probe(configurations, black_box_function, logger, verbose=True):
     experiments = gp_minimize(
         func=black_box_function,
         dimensions=search_space,
-        acq_func="EI", # [LCB, EI, PI]
+        # acq_func="EI", # [LCB, EI, PI]
         # acq_optimizer="lbfgs", # [sampling, lbfgs]
         n_calls=configurations["thread"]["iteration"],
         n_random_starts=configurations["thread"]["random_probe"],
@@ -29,7 +29,7 @@ def initial_probe(configurations, black_box_function, logger, verbose=True):
         y0=None,
         verbose=verbose,
         # callback=None,
-        xi=0.01, # EI or PI
+        # xi=0.01, # EI or PI
         # kappa=1.96, # LCB only
     )
     
@@ -48,7 +48,7 @@ def repetitive_probe(configurations, black_box_function, logger, verbose=True):
     experiments = gp_minimize(
         func=black_box_function,
         dimensions=search_space,
-        acq_func="EI", # [LCB, EI, PI]
+        # acq_func="EI", # [LCB, EI, PI]
         # acq_optimizer="lbfgs", # [sampling, lbfgs]
         n_calls=configurations["thread"]["iteration"],
         n_random_starts=configurations["thread"]["random_probe"],
@@ -57,7 +57,7 @@ def repetitive_probe(configurations, black_box_function, logger, verbose=True):
         y0=None,
         verbose=verbose,
         # callback=None,
-        xi=0.01, # EI or PI
+        # xi=0.01, # EI or PI
         # kappa=1.96, # LCB only
     )
     
