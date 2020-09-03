a4d8581acb4038a3410ebb0c525d7123b37ad744
Md Arifuzzaman <arif@nevada.unr.edu>
2020-09-03 16:21:37 -0700

    new

--- sender.py
+++ sender.py
@@ -146,29 +146,29 @@ def worker(indx):
                                         pass
                                     
                                     timer100ms = time.time()
-                            
-                            # if calculate_stats[indx] == 1:
-                            #     sc, rc = tcp_stats(addr)
-                            #     segments_sent.value += sc
-                            #     segments_retransmitted.value += rc
-                            #     calculate_stats[indx] = 0
-
-                            duration = time.time() - start
-                            if (sample_phase.value == 1 and (duration > probing_time)) or (process_status[indx] == 0):
-                                if sent == 0:
-                                    transfer_status[i] = 1
-                                    log.debug("finished {0}, {1}, {2}".format(indx, i, filename))
                                     
-                                break
+                            if calculate_stats[indx] == 1:
+                                sc, rc = tcp_stats(addr)
+                                segments_sent.value += sc
+                                segments_retransmitted.value += rc
+                                calculate_stats[indx] = 0
+
+                            # duration = time.time() - start
+                            # if (sample_phase.value == 1 and (duration > probing_time)) or (process_status[indx] == 0):
+                            #     if sent == 0:
+                            #         transfer_status[i] = 1
+                            #         log.debug("finished {0}, {1}, {2}".format(indx, i, filename))
+                                    
+                            #     break
                             
                             if sent == 0:
                                 transfer_status[i] = 1
                                 log.debug("finished {0}, {1}, {2}".format(indx, i, filename)) 
                                 break
                 
-                sc, rc = tcp_stats(addr)
-                segments_sent.value += sc
-                segments_retransmitted.value += rc
+                # sc, rc = tcp_stats(addr)
+                # segments_sent.value += sc
+                # segments_retransmitted.value += rc
                 # lr = rc/sc if sc>0 else 0
                 # log.info("Process: {0}, Loss Rate: {1}".format(indx+1, np.round(lr, 4)))
                 process_status[indx] = 0
@@ -211,43 +211,43 @@ def sample_transfer(params):
         params[0] = len(file_names)
         log.info("Effective Concurrency: {0}".format(num_workers.value))
 
-    # for i in range(configurations["thread_limit"]):
-    #     if i < params[0]:
-    #         process_status[i] = 1
-    #     else:
-    #         process_status[i] = 0
+    for i in range(configurations["thread_limit"]):
+        if i < params[0]:
+            process_status[i] = 1
+        else:
+            process_status[i] = 0
 
-    # time.sleep(0.1)
+    time.sleep(0.1)
     num_workers.value = params[0]
     chunk_size.value = get_buffer_size(params[1])
 
     start_time = time.time()
     score_before = np.sum(file_offsets)
 
-    # for i in range(params[0]):
-    #     calculate_stats[i] = 1
+    for i in range(params[0]):
+        calculate_stats[i] = 1
     
-    # while np.sum(calculate_stats) > 0:
-    #     pass
+    while np.sum(calculate_stats) > 0:
+        pass
 
     before_sc, before_rc = segments_sent.value, segments_retransmitted.value
 
-    for i in range(params[0]):
-        process_status[i] = 1
+    # for i in range(params[0]):
+    #     process_status[i] = 1
     
     time.sleep(probing_time)
 
-    # for i in range(params[0]):
-    #     calculate_stats[i] = 1
+    for i in range(params[0]):
+        calculate_stats[i] = 1
     
-    # while np.sum(calculate_stats) > 0:
-    #     pass
+    while np.sum(calculate_stats) > 0:
+        pass
 
-    while np.sum(process_status)>0:
-        for i in range(params[0]):
-            process_status[i] = 0
+    # while np.sum(process_status)>0:
+    #     for i in range(params[0]):
+    #         process_status[i] = 0
         
-        time.sleep(0.01)
+    #     time.sleep(0.01)
 
     score_after = np.sum(file_offsets)
     after_sc, after_rc = segments_sent.value, segments_retransmitted.value
