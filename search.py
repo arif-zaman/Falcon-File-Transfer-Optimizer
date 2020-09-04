d4c11b1d9b60439087fbc75f150bdba84c32f111
Md Arifuzzaman <arif@nevada.unr.edu>
2020-09-04 14:54:46 -0700

    test

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ import time
 
 
 def base_optimizer(configurations, black_box_function, logger, verbose=True):
-    limit_obs = 10  
+    limit_obs = 15  
     search_space  = [
         Integer(configurations["thread"]["min"], configurations["thread"]["max"]),
         Integer(1, configurations["chunk_limit"])
@@ -20,8 +20,8 @@ def base_optimizer(configurations, black_box_function, logger, verbose=True):
     experiments = Optimizer(
         dimensions=search_space,
         n_initial_points=configurations["thread"]["random_probe"],
-        acq_func="LCB",
-        acq_func_kwargs={"kappa":5},
+        # acq_func="LCB",
+        # acq_func_kwargs={"kappa":5},
         model_queue_size=limit_obs
     )
 
