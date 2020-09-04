d630639f37c21f368b8e096351b657056a3c948d
Md Arifuzzaman <arif@nevada.unr.edu>
2020-09-03 17:48:51 -0700

    new

--- sender.py
+++ sender.py
@@ -156,22 +156,22 @@ def worker(indx):
                             #     calculate_stats[indx] = 0
                             
 
-                            duration = time.time() - start
-                            if (sample_phase.value == 1 and (duration > probing_time)):
-                                if sent == 0:
-                                    transfer_status[i] = 1
-                                    log.debug("finished {0}, {1}, {2}".format(indx, i, filename))
+                            # duration = time.time() - start
+                            # if (sample_phase.value == 1 and (duration > probing_time)):
+                            #     if sent == 0:
+                            #         transfer_status[i] = 1
+                            #         log.debug("finished {0}, {1}, {2}".format(indx, i, filename))
                                     
-                                process_status[indx] = 0
+                            #     process_status[indx] = 0
                             
                             if sent == 0:
                                 transfer_status[i] = 1
                                 log.debug("finished {0}, {1}, {2}".format(indx, i, filename)) 
                                 break
                 
-                sc, rc = tcp_stats(addr)
-                segments_sent.value += sc
-                segments_retransmitted.value += rc
+                # sc, rc = tcp_stats(addr)
+                # segments_sent.value += sc
+                # segments_retransmitted.value += rc
                 # lr = rc/sc if sc>0 else 0
                 # log.info("Process: {0}, Loss Rate: {1}".format(indx+1, np.round(lr, 4)))
                 process_status[indx] = 0
@@ -214,13 +214,13 @@ def sample_transfer(params):
         params[0] = len(file_names)
         log.info("Effective Concurrency: {0}".format(num_workers.value))
 
-    # for i in range(configurations["thread_limit"]):
-    #     if i < params[0]:
-    #         process_status[i] = 1
-    #     else:
-    #         process_status[i] = 0
+    for i in range(configurations["thread_limit"]):
+        if i < params[0]:
+            process_status[i] = 1
+        else:
+            process_status[i] = 0
 
-    # time.sleep(1)
+    time.sleep(1)
     num_workers.value = params[0]
     chunk_size.value = get_buffer_size(params[1])
 
@@ -233,12 +233,12 @@ def sample_transfer(params):
     # while np.sum(calculate_stats) > 0:
     #     pass
 
-    before_sc, before_rc = segments_sent.value, segments_retransmitted.value
+    # before_sc, before_rc = segments_sent.value, segments_retransmitted.value
 
-    for i in range(params[0]):
-        process_status[i] = 1
+    # for i in range(params[0]):
+    #     process_status[i] = 1
     
-    # time.sleep(probing_time)
+    time.sleep(probing_time)
 
     # for i in range(params[0]):
     #     calculate_stats[i] = 1
@@ -246,19 +246,19 @@ def sample_transfer(params):
     # while np.sum(calculate_stats) > 0:
     #     pass
 
-    while np.sum(process_status)>0:
-        pass
+    # while np.sum(process_status)>0:
+    #     pass
     
     duration = time.time() - start_time
     score_after = np.sum(file_offsets)
-    after_sc, after_rc = segments_sent.value, segments_retransmitted.value
+    # after_sc, after_rc = segments_sent.value, segments_retransmitted.value
 
     score = score_after - score_before
-    sc, rc = after_sc - before_sc, after_rc - before_rc        
+    # sc, rc = after_sc - before_sc, after_rc - before_rc        
     thrpt = (score * 8) / (duration*1000*1000)
     lr, C = 0, int(configurations["C"])
-    if sc != 0:
-        lr = rc/sc if sc>rc else 0
+    # if sc != 0:
+    #     lr = rc/sc if sc>rc else 0
     
     score_value = thrpt #* (1 - C * ((1/(1-lr))-1)) 
     score_value = np.round(score_value * (-1), 4)
